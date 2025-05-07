4fb24a5130179eb26ac1d10f74f7f2b5fae312c9
Andrey Brezgalov <tyrewe228@gmail.com>
2025-05-07 03:43:40 +0300

    feat: додано запит до API

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ async function loadConfig(filename) {
         throw error;
     }
 }
+
+async function getDataFromApi(apiKey, city) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`;
+    try {
+        const response = await fetch(url);
+        if (response.status === 200) {
+            return await response.json();
+        } else {
+            throw new Error(`Помилка API: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Помилка запиту до API:', error.message);
+        throw error;
+    }
+}
